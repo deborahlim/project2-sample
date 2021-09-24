6142853acd2a59499532399c25daaa98b769f2fd
deborahlim <deborahlimhy@gmail.com>
2021-09-24 04:41:50 +0000

    update URL

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://dlhy-tgc-special-connections.herokuapp.com/";
+const URL = "https://dlhy-tgc-special-connections.herokuapp.com";
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
